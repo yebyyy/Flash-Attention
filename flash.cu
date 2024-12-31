9c594b8deb65578b73b81fb6deda2d9ca09a8ab2
yebyyy <yexiaomeng2004@gmail.com>
2024-12-30 21:52:06 -0500

    add

---
+++ flash.cu
@@ -0,0 +1,8 @@
+#include <torch/torch.h>
+#include <torch/script.h>
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <sys/timeb.h>
+#include <device_launch_parameters.h>
+
